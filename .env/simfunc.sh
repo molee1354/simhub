011e9aec862c5965f1361660b8d0173ae99e5044
molee1354 <molee1354@gmail.com>
2023-05-26 16:51:24 +0900

    simulations functions update

---
+++ .env/simfunc.sh
@@ -0,0 +1,27 @@
+function sim() {
+    case $1 in
+        run)
+            cd ./simulation
+            ./main
+            cd ../
+            ;;
+        anim)
+            if [[ ! -d "./simulation/vis/" ]]; then
+                echo "No visualization module found in current simulation!"
+                return 0
+            fi
+            python3 ./simulation/vis/animate.py ./simulation/dump.out
+            ;;
+        plot)
+            if [[ ! -d "./simulation/vis/" ]]; then
+                echo "No visualization module found in current simulation!"
+                return 0
+            fi
+            python3 ./simulation/vis/plot.py ./simulation/dump.out
+
+            ;;
+        *)
+            echo "Not a valid input"
+            ;;
+    esac
+}

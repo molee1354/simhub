86c76baf1ab8f6e5ba973b7037cd20791228b5f5
molee1354 <molee1354@gmail.com>
2023-06-13 04:27:05 +0900

    added todo

--- source/utils/array/main.c
+++ source/utils/array/main.c
@@ -6,8 +6,17 @@
 #define TYPE int
 
 /* TODO:
+ *
  * write out functions that can test the different array operations
  * and make sure they cover edge cases.
+ *      -> out of index behaviors
+ *      -> doing different things to an empty array
+ * 
+ * Also maybe make it so that methods for `stack` and `queue` data
+ * structures can also be used:
+ *      -> `stack`: push(), pop(), peek()
+ *      -> `queue`: enqueue(), dequeue(), peek()
+ *
  */
 
 int main() {

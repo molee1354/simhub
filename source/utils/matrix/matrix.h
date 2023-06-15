c027be42372bd384c2fc68614fc2d2e154430af5
molee1354 <molee1354@gmail.com>
2023-06-16 01:49:59 +0900

    matrix ideas

--- source/utils/matrix/matrix.h
+++ source/utils/matrix/matrix.h
@@ -1,23 +1,38 @@
 #ifndef _matrix_h
 #define _matrix_h
 
-typedef struct Matrix {
-    int matSize[2]; // 0 --> row, 1 --> col
-    int rows;
-    int cols;
-    float** elements;
-} Obsolete;
+
+/* 
+ * Change this to "array.h" later,
+ * or perhaps make a shared library
+ */
+#include "../array/src/array.h"
 
 /*
- * New Stuff
+ * Define matrix struct with Array objects
  */
-typedef struct BetterMatrix {
+typedef struct ArrayMatrix {
     Array** rowsNcols; // all the rows and columns as arrays
     int matSize[2];
     int rows;
     int cols;
-    double (*getFrom)(char*);
-} Matrix;
+    Array (*getElement)(int, int);
+    Array* (*getArray)(char*);
+    struct ArrayMatrix* (*getSlice)(char*);
+} Matrix_A;
+
+/*
+ * Define matrix struct with double
+ */
+typedef struct DoubleMatrix {
+    double** rowsNcols; // all the rows and columns as arrays
+    int matSize[2];
+    int rows;
+    int cols;
+    double (*getElement)(int, int);
+    double* (*getArray)(char*);
+    struct DoubleMatrix** (*getSlice)(char*);
+} Matrix_d;
 
 /*
  * Function to make a new matrix
@@ -35,14 +50,30 @@ typedef struct BetterMatrix {
  *         out->getFrom = &functionName;
  *         ...
  *
+ *         -> and for the Array** double pointer, every single row and 
+ *              column will have its own array representation.
+ *              -> it could still be possible to make it a double** as well?
+ *              -> perhaps rows already exist, and I just have to make the
+ *                  column arrays
+ *
+ *     -> Also, multiple dispatch may be a bad idea since matrix stuff like invert
+ *          will mess with it.
+ *         -> Maybe just support add() subtract() constMult() and matMult()
+ *
+ */
+Matrix* makeMatrix( double** nMatrix, int numRows, int numCols );
+
+/*
+ * Functions to slice or index
  */
 
-/* new stuff above */
 
-//TODO -> fix this
-int** initMatrix( int row, int col, int array2d[][col] );
-
-Matrix* old_makeMatrix( int row, int col, float** matPointer );
+/*
+ * Common matrix generators
+ */
+Matrix* zeros( int numRows, int numCols );
+Matrix* ones( int numRows, int numCols );
+Matrix* eye( int numRows, int numCols );
 
 int* getSize( Matrix* matrix );
 int getElement( Matrix* matrix);

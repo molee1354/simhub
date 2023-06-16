e8d0eb8fbbace77ee374bb1b8262dffacea4df41
molee1354 <molee1354@gmail.com>
2023-06-16 12:48:56 +0900

    opaque type implementation

--- source/utils/matrix/src/guard.h
+++ source/utils/matrix/src/guard.h
@@ -1,1 +1,23 @@
 #include "matrix.h"
+
+
+/*
+ * Define vector struct
+ *      -> enums used to set direction
+ */
+typedef struct DoubleVector {
+    double* element;
+    vType direction;
+} Vector;
+
+/*
+ * Define matrix struct with double
+ * the length of a row is equal to the number of columns and vice versa
+ */
+typedef struct DoubleMatrix {
+    Vector* rowsNcols; // all the rows and columns as vectors
+    int rows;
+    int cols;
+    int size[2];
+    int squareMat;
+} Matrix;

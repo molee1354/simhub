2dbb8706fd3fa6fd21364a1269b5e3867d9fb7c2
molee1354 <molee1354@gmail.com>
2023-06-19 20:13:58 +0900

    main update (segfault)

--- source/utils/matrix/src/main.c
+++ source/utils/matrix/src/main.c
@@ -3,23 +3,27 @@
 
 #include "matrix.h"
 
-#define NROW 8
-#define NCOL 10
+#define NROW 3
+#define NCOL 3
+
 int main() {
-    double** nMatrix = (double**)malloc(sizeof(double*)*NROW);
-    for (int i=0; i<NROW; i++) {
-        nMatrix[i] = (double*)malloc(sizeof(double)*NCOL);
-    }
-    int n = 1;
-    for (int i=0; i<NROW; i++) {
-        for (int j=0; j<NCOL; j++) {
-            nMatrix[i][j] = n++;
-        }
-    }
+    // double** nMatrix = (double**)malloc(sizeof(double*)*NROW);
+    // for (int i=0; i<NROW; i++) {
+    //     nMatrix[i] = (double*)malloc(sizeof(double)*NCOL);
+    // }
+    // int n = 1;
+    // for (int i=0; i<NROW; i++) {
+    //     for (int j=0; j<NCOL; j++) {
+    //         nMatrix[i][j] = n++;
+    //     }
+    // }
 
     // Matrix* myMatrix = toMatrix(nMatrix, NROW, NCOL);
     // Matrix* myMatrix = ones(NROW, NCOL);
     Matrix* myMatrix = eye(NCOL);
+    double** modMatrix = makeModifiable(myMatrix);
+    modMatrix[1][1] = -1.;
+
     puts("\nprintMatrix()");
     printMatrix(myMatrix);
 
@@ -39,6 +43,9 @@ int main() {
 
     puts("\nfreeMatrix()");
     freeMatrix(myMatrix);
+
+    puts("\nfreeModifiable()");
+    freeModifiable(modMatrix, NCOL);
 
     return 0;
 }

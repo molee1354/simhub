b72269461f7fb490bd3ec1079b179e0b1defc5b6
molee1354 <molee1354@gmail.com>
2023-06-22 14:15:54 +0900

    const mult for matrix

---
+++ source/utils/matrix/src/mult_C.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "matrix.h"
+#include "guard.h"
+
+/*
+ * All math operations will create a new matrix
+ */
+Matrix* mult_MC( Matrix* matrix, double constant ) {
+    Matrix* out = makeMatrix( matrix->nRows, matrix->nCols );
+    for (int i = 0; i<matrix->nRows; i++) {
+        for (int j = 0; j<matrix->nCols; j++) {
+            out->e[i][j] = matrix->e[i][j]*constant;
+        }
+    }
+    puts("Make sure to free the memory allocated by the mult_C() function call");
+    return out;
+}
+Vector* mult_VC( Vector* vector, double constant ) {
+    Vector* out = makeVector( vector->nEle, vector->direction );
+    for (int i = 0; i<vector->nEle; i++) {
+        out->e[i] = vector->e[i]*constant;
+    }
+    puts("Make sure to free the memory allocated by the mult_C() function call");
+    return out;
+}
+

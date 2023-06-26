205ae5ec720cdb394d3758d7d4b4ed4510d7cd6f
molee1354 <molee1354@gmail.com>
2023-06-26 21:36:25 +0900

    copyVector comment consistantcy

--- source/utils/matrix/src/copyVector.c
+++ source/utils/matrix/src/copyVector.c
@@ -4,7 +4,7 @@
 
 Vector* copyVector(Vector* vector) {
     Vector* out = makeVector(vector->nEle, vector->direction);
-    puts("Make sure to free the memory allocated by the copyMatrix() function call");
+    puts("Make sure to free the memory allocated by the copyVector() function call");
     for (int i = 0; i < vector->nEle; i++) {
         out->e[i] = vector->e[i];
     }

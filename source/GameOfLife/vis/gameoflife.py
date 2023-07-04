9327d8e372cd20dade9a0c4ee17fcff50fb1e529
molee1354 <molee1354@gmail.com>
2023-07-04 14:41:53 +0900

    headerfile edit

--- source/GameOfLife/vis/gameoflife.py
+++ source/GameOfLife/vis/gameoflife.py
@@ -1,2 +1,8 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
+import os
+
+
+try:
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+except ModuleNotFoundError:
+    os.system("pip3 install -r requirements.txt")

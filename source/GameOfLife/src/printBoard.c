01203cfb042ae811bfbf02b9d6378aabc6d747f4
molee1354 <molee1354@gmail.com>
2023-07-04 21:38:16 +0900

    removed comment

--- source/GameOfLife/src/printBoard.c
+++ source/GameOfLife/src/printBoard.c
@@ -4,7 +4,6 @@
 void printBoard( Board* board ) {
     for (int i=0; i<board->numRows; i++) {
         for (int j=0; j<board->numCols; j++) {
-            // printf("%d ", board->boardMatrix[i][j]);
             if (board->boardMatrix[i][j]==1) printf("■ ");
             else printf("□ ");
         }

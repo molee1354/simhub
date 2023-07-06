accdc236c7a0940f7008dc469f89b04e7b0b187b
molee1354 <molee1354@gmail.com>
2023-07-07 04:06:59 +0900

    write per timestep

--- source/GameOfLife/src/main.c
+++ source/GameOfLife/src/main.c
@@ -21,22 +21,23 @@ int main() {
         nextBoard = generateNext;
     }
 
-    FILE *out;
-    out = fopen(DATA_OUT, "w");
-    if (out == NULL) {
-        puts("File error");
-        exit(1);
-    }
 
     double start = omp_get_wtime();
     for (int i = 0; i<SIM_END; i++) {
+        FILE *out;
+        char filename[32];
+        snprintf(filename, sizeof(filename), "dumps/dump_%d.out", i);
+        out = fopen(filename, "w");
+        if (out == NULL) {
+            puts("File error");
+            exit(1);
+        }
+
         nextBoard(board);
         writeBoard(out, board, i);
         printf("\t%d\n", i);
-        // printBoard(board);
-        // puts("");
-        // sleep(1);
-        // system("clear");
+
+        fclose(out);
     }
     clock_t end = omp_get_wtime();
     double elapse_time = end-start;
@@ -44,7 +45,6 @@ int main() {
     printf("\nElapsed time %.3f sec\n", elapse_time);
     printf("\nFinished running GameOfLife simulation for %d timesteps\n\n", SIM_END);
     freeBoard(board);
-    fclose(out);
 
     return 0;
 }

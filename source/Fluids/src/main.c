7b46987d779044159a585c9b98cbf4f38662c25d
Mokin Lee <moosung1354@gmail.com>
2023-09-13 03:20:20 +0900

    print for fields

--- source/Fluids/src/main.c
+++ source/Fluids/src/main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #include "fluid.h"
 #include "sim.input"
 
 int main(int argc, char* argv[]) {
-    initFluid(DENSITY, NUM_X, NUM_Y, DEF_H);
+    Fluid* fluid = initFluid(DENSITY, NUM_X, NUM_Y, DEF_H);
 
-    simulate(DT, GRAVITY, NUM_ITER);
+    for (int i = 0; i < 50; i++) {
+
+        simulate(DT, GRAVITY, NUM_ITER);
+        sleep(1);
+    }
     puts("Done.!");
 
     return 0;

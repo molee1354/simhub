5ee4b3866754bff45ff4d0e50ab4ee6f27d08aae
Mokin Lee <molee1354@gmail.com>
2024-01-11 18:59:56 +0900

    added gdb debugger script

---
+++ .env/debugger.py
@@ -0,0 +1,35 @@
+import gdb
+
+def debugger(filename: str) -> None:
+    print(f"Setting current file as {filename}...")
+    gdb.execute("file "+filename)
+    gdb.execute("set pagination off")
+    gdb.execute("set print pretty on")
+    gdb.execute("set verbose off")
+    # gdb.execute("break src/FLIP_fluid.c:159")
+    gdb.execute("run")
+
+    # while True:
+    #     id = gdb.parse_and_eval('id')
+    #     qx = gdb.parse_and_eval('qx')
+    #     max_p = gdb.parse_and_eval('fluid->maxParticles')
+    #     print(f"id    = {id}")
+    #     gdb.execute("continue")
+    #     if int(id) > 100:
+    #         break;
+    id = gdb.parse_and_eval('id')
+    qx = gdb.parse_and_eval('qx')
+    max_p = gdb.parse_and_eval('fluid->maxParticles')
+    print("id larger than max_p")
+    print(f"max_p = {max_p}")
+    print(f"id    = {id}")
+    print(f"qx    = {qx}")
+
+def main() -> None:
+    path = "simulation/bin/main"
+    debugger(path)
+    # gdb.execute('quit')
+
+
+if __name__ == "__main__":
+    main()
